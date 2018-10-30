31c92f85f23500621c6cdf120a33f95b04b19c5e
hongbiao <qq@694993708.com>
2018-10-30 20:36:29 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDom from 'react-dom';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {Button} from 'antd-mobile';
+
+ReactDom.render(<Button type="primary">按钮</Button>,document.getElementById('root'));
